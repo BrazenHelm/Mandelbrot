01b8db261046c7a64957e9ef84a596040e79dff4
Brazen_Helm <stephenaidanjones@gmail.com>
2019-10-09 17:06:18 +0100

    copied main functions into FractalMaker methods

--- Fractals/FractalMaker.cpp
+++ Fractals/FractalMaker.cpp
@@ -1,12 +1,71 @@
 #include "FractalMaker.h"
+#include "Mandelbrot.h"
+#include <iostream>
+#include <numeric>
+
+namespace BHFractals {
+
+	FractalMaker::FractalMaker(int width, int height) :
+		bitmap(width, height),
+		histogram(new int[Mandelbrot::MAX_ITERATIONS]{ 0 }),
+		table(new int[width * height]) {
+	}
+	
+	
+	void FractalMaker::calculateIterations() {
+
+		for (int x = 0; x < Settings::WIDTH; ++x) {
+			for (int y = 0; y < Settings::HEIGHT; ++y) {
+				double xScaled = ScaleX(x);
+				double yScaled = ScaleY(y);
+				int iterations = Mandelbrot::getIterations(xScaled, yScaled);
+				if (iterations != Mandelbrot::MAX_ITERATIONS) {
+					++histogram[iterations];
+				}
+				table[x + y * Settings::WIDTH] = iterations;
+			}
+		}
+
+		for (int i = Mandelbrot::MAX_ITERATIONS - 1; i >= 0; --i) {
+			histogram[i] = std::accumulate(histogram.get(), histogram.get() + i + 1, 0);
+		}
+
+	}
 
 
+	void FractalMaker::drawFractal() {
+		
+		Color light = Color::Green();
+		Color dark = Color::Black();
+		Color inside = Color::Black();
 
-FractalMaker::FractalMaker()
-{
-}
+		for (int x = 0; x < Settings::WIDTH; ++x) {
+			for (int y = 0; y < Settings::HEIGHT; ++y) {
+				int iterations = table[x + y * Settings::WIDTH];
+				Color color = inside;
+
+				if (iterations != Mandelbrot::MAX_ITERATIONS) {
+					double t = static_cast<double>(histogram[iterations]) / histogram[Mandelbrot::MAX_ITERATIONS - 1];
+					t = pow(255, t) / 255;
+					color = Color::Lerp(dark, light, t);
+				}
+
+				bitmap.setPixel(x, y, color);
+			}
+		}
+
+	}
 
 
-FractalMaker::~FractalMaker()
-{
+	bool FractalMaker::writeBitmap(std::string filename) {
+		if (bitmap.write(filename)) {
+			std::cout << "Finished writing to " << filename << std::endl;
+			return true;
+		}
+		else {
+			std::cout << "Failed to write to " << filename << std::endl;
+			return false;
+		}
+	}
+
 }
--- Fractals/FractalMaker.h
+++ Fractals/FractalMaker.h
@@ -1,8 +1,21 @@
 #pragma once
-class FractalMaker
-{
-public:
-	FractalMaker();
-	~FractalMaker();
-};
+#include "Bitmap.h"
+#include "Settings.h"
+#include <string>
 
+namespace BHFractals {
+
+	class FractalMaker {
+	private:
+		Bitmap bitmap;
+		std::unique_ptr<int[]> histogram;
+		std::unique_ptr<int[]> table;
+	public:
+		FractalMaker(int width, int height);
+
+		void calculateIterations();
+		void drawFractal();
+		bool writeBitmap(std::string filename);
+	};
+
+}
