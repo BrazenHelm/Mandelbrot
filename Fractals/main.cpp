70d8ebe2df7084b8f7ffcac1300e487d5f3a6bc5
Brazen_Helm <stephenaidanjones@gmail.com>
2019-10-09 15:16:09 +0100

    store all required data

--- Fractals/main.cpp
+++ Fractals/main.cpp
@@ -11,10 +11,11 @@ int main() {
 	Bitmap bitmap(Settings::WIDTH, Settings::HEIGHT);
 
 	Color light = Color(1.00, 0.08, 0.58);
-	Color dark = Color::Black();
-	Color inside = Color::Black();
+	Color dark = Color::White();
+	Color inside = Color::White();
 
-	std::unique_ptr<int[]> histogram(new int[Mandelbrot::MAX_ITERATIONS + 1]{ 0 });
+	std::unique_ptr<int[]> histogram(new int[Mandelbrot::MAX_ITERATIONS]{ 0 });
+	std::unique_ptr<int[]> table(new int[Settings::WIDTH * Settings::HEIGHT]);
 
 	for (int x = 0; x < Settings::WIDTH; ++x) {
 		if (x % 50 == 0) {
@@ -24,15 +25,23 @@ int main() {
 			double xScaled = ScaleX(x);
 			double yScaled = ScaleY(y);
 			int iterations = Mandelbrot::getIterations(xScaled, yScaled);
+			if (iterations != Mandelbrot::MAX_ITERATIONS) {
+				++histogram[iterations];
+			}
+			table[x + y * Settings::WIDTH] = iterations;
+		}
+	}
 
-			++histogram[iterations];
-			
-			//Color color = inside;
-			//if (iterations != Mandelbrot::MAX_ITERATIONS) {
-			//	double t = static_cast<double>(iterations) / Mandelbrot::MAX_ITERATIONS;
-			//	color = Color::Lerp(dark, light, t);
-			//}
-			//bitmap.setPixel(x, y, color);
+	for (int x = 0; x < Settings::WIDTH; ++x) {
+		for (int y = 0; y < Settings::HEIGHT; ++y) {
+			int iterations = table[x + y * Settings::WIDTH];
+
+			Color color = inside;
+			if (iterations != Mandelbrot::MAX_ITERATIONS) {
+				double t = static_cast<double>(iterations) / Mandelbrot::MAX_ITERATIONS;
+				color = Color::Lerp(dark, light, t);
+			}
+			bitmap.setPixel(x, y, color);
 		}
 	}
 
@@ -40,15 +49,14 @@ int main() {
 		std::cout << histogram[i] << " ";
 	}
 	std::cout << std::endl;
-	return 0;
 
-	//const std::string FILENAME = "image.bmp";
-	//if (bitmap.write(FILENAME)) {
-	//	std::cout << "Finished writing to " << FILENAME << std::endl;
-	//	return 0;
-	//}
-	//else {
-	//	std::cout << "Failed to write to " << FILENAME << std::endl;
-	//	return 1;
-	//}
+	const std::string FILENAME = "image.bmp";
+	if (bitmap.write(FILENAME)) {
+		std::cout << "Finished writing to " << FILENAME << std::endl;
+		return 0;
+	}
+	else {
+		std::cout << "Failed to write to " << FILENAME << std::endl;
+		return 1;
+	}
 }
